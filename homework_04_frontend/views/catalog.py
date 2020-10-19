423bbf5b02fc9482cf8180b8700cce335e8c66d1
Ilya Omelyukhin <i.omelyuhin@hmail.com>
2020-10-19 21:03:06 +0300

    add products list

--- homework_04_frontend/views/catalog.py
+++ homework_04_frontend/views/catalog.py
@@ -2,7 +2,12 @@ from flask import Blueprint, render_template
 
 catalog_app = Blueprint("catalog_app", __name__)
 
+PRODUCTS = {
+    1: "Развивающие книги",
+    2: "Конструкторы",
+    3: "Раскраски"
+}
 
 @catalog_app.route("/catalog")
 def catalog_list():
-    return render_template("catalog/index.html")
+    return render_template("catalog/index.html", products=PRODUCTS)
