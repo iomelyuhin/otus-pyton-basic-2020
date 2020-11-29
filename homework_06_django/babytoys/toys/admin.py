a82d9210138a74fd4982639391ee0184e36b9850
Ilya Omelyukhin <i.omelyuhin@hmail.com>
2020-11-29 13:05:36 +0300

    add index tests.py

--- homework_06_django/babytoys/toys/admin.py
+++ homework_06_django/babytoys/toys/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
-from .models import Category, Card, Images, OrderItem, Order
+from .models import Category, Card, Images
 
 admin.site.register(Card)
 admin.site.register(Category)
 admin.site.register(Images)
-admin.site.register(OrderItem)
-admin.site.register(Order)
 
 # Register your models here.
--- homework_06_django/babytoys/toys/tests.py
+++ homework_06_django/babytoys/toys/tests.py
@@ -1,5 +1,5 @@
-from django.test import TestCase
-from .models import Category
+from django.test import TestCase, Client
+from .models import Category, Card
 
 
 class TestCategories(TestCase):
@@ -8,7 +8,26 @@ class TestCategories(TestCase):
         self.category = Category.objects.create(name="Деревянные игрушки")
 
     def tearDown(self):
-        pass
+        print('Done')
 
     def test_str(self):
         self.assertEqual(str(self.category), "Деревянные игрушки")
+
+
+class TestView(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.category = Category.objects.create(name="Деревянные игрушки")
+
+    def tearDown(self):
+        print('Done')
+
+    def test_index_menu(self):
+        response = self.client.get("")
+        text = response.content.decode(encoding='utf-8')
+        self.assertTrue(str(self.category) in text)
+
+    def test_index_context(self):
+        response = self.client.get("")
+        self.assertTrue('categories' in response.context)
